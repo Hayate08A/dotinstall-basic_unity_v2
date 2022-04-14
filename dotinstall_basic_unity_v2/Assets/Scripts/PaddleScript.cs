185434ff321a6f7ac54cc72d9e760e8c72f98209
adachi.h@marietta.co.jp <adachi.h@marietta.co.jp>
2022-04-14 19:10:12 +0900

    [Add] ゲームを動かしてみよう

--- dotinstall_basic_unity_v2/Assets/Scripts/PaddleScript.cs
+++ dotinstall_basic_unity_v2/Assets/Scripts/PaddleScript.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class PaddleScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  // Start is called before the first frame update
+  void Start()
+  {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime, 0f, 0f);
+    Debug.Log(transform.position.x);
+  }
 }
