5359c7c24b74b15bb49e55eee2ade66fe1988773
Ha08ya <ha08ya@gmail.com>
2022-04-15 12:53:56 +0900

    [Add] 一定時間ごとにボールを生成しよう

--- dotinstall_basic_unity_v2/Assets/Scripts/BallFactoryScript.cs
+++ dotinstall_basic_unity_v2/Assets/Scripts/BallFactoryScript.cs
@@ -8,11 +8,13 @@ public class BallFactoryScript : MonoBehaviour
   // Start is called before the first frame update
   void Start()
   {
-    Instantiate(ball, transform.position, transform.rotation);
-    // InvokeRepeating();
+    InvokeRepeating("SpawnBall", 0f, 1f);
   }
 
-  void SpawnBall() { }
+  void SpawnBall()
+  {
+    Instantiate(ball, new Vector3(Random.Range(-5f, 5f), transform.position.y, transform.position.z), transform.rotation);
+  }
 
   // Update is called once per frame
   void Update()
